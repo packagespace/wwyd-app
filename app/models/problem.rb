4e19e78ee8dafafbe2bb177a7d0cd4ab60b786f1
packagespace <packagespace@pm.me>
2023-11-18 18:03:56 +0100

    Allow formatting hands

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,12 @@
 class Problem < ApplicationRecord
+  def tiles
+    self[:hand].gsub(%r{(?<numbers>[0-9']*)(?<suit>[mpsz])}) do |_|
+      result = ''
+      numbers, suit = $1, $2
+      numbers.split(%r{(?!')}).each do |number|
+        result += "#{number}#{suit}"
+      end
+      result
+    end
+  end
 end
--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return tiles with suits added" do
+    # Arrange
+    problem = Problem.new(hand: "44m667p123678s 666'z")
+
+    # Act
+    tiles = problem.tiles
+
+    # Assert
+    assert_equal "4m4m6p6p7p1s2s3s6s7s8s 6z6z6'z", tiles
+  end
 end
