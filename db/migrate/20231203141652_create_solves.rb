728ab725e254f4131c315133044d83114920c356
packagespace <packagespace@pm.me>
2023-12-03 20:17:46 +0100

    Make user nullable

--- db/migrate/20231203141652_create_solves.rb
+++ db/migrate/20231203141652_create_solves.rb
@@ -3,7 +3,7 @@ class CreateSolves < ActiveRecord::Migration[7.1]
     create_table :solves do |t|
       t.string :tile
       t.references :problem, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_141652) do
   create_table "solves", force: :cascade do |t|
     t.string "tile"
     t.integer "problem_id", null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["problem_id"], name: "index_solves_on_problem_id"
