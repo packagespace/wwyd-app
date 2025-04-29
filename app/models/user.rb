c5466f71e9fca662b2054b3bddf16be30c312e31
packagespace <packagespace@pm.me>
2025-04-29 19:53:24 +0200

    Add minimum length constraint to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  #todo add min password length
+  validates :password, length: { minimum: 8}
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
