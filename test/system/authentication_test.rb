e83c1bdc40bff35090f3a8674645b1a9697cf644
packagespace <packagespace@pm.me>
2025-05-04 21:18:33 +0200

    Fix test

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -31,7 +31,7 @@ class AuthenticationTest < ApplicationSystemTestCase
 
     click_button "Sign out"
 
-    assert_current_path new_session_path
+    assert_current_path root_path
     assert_no_text users(:one).email_address
     assert_text "Sign in"
     assert_text "Sign up"
