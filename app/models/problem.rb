7914193b44e2d20900a00dfd42db0de1145f21fa
packagespace <packagespace@pm.me>
2025-05-12 09:44:08 +0200

    Fix tests

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,12 +1,5 @@
 class Problem < ApplicationRecord
   has_many :solves, dependent: :destroy
-  attribute :hand_tiles
-  attribute :solution_tiles
-  after_initialize :parse_hand_tiles, :parse_solution_tiles
-
-  def hand_tiles_to_s
-    hand_tiles.join
-  end
 
   def is_solved_by?(answer)
     solution.gsub(%r{(?<numbers>[0-9']*)(?<suit>[mpsz])}) do |_|
@@ -18,24 +11,21 @@ class Problem < ApplicationRecord
     false
   end
 
-  def solution_tiles_to_s
-    self.solution_tiles.join(" or ")
+
+  def hand_tiles
+    parse_tiles(hand)
+  end
+
+  def solution_tiles
+    parse_tiles(solution)
   end
 
   private
-  def parse_hand_tiles
-    self.hand_tiles = parse_tiles(hand)
-    end
-
-  def parse_solution_tiles
-    self.solution_tiles = parse_tiles(solution)
-  end
-
   def parse_tiles(input)
-    input.scan(/([1-9]+)([mpsz])/).map do |numbers, suit|
-      numbers.chars.map do |number|
-        Tile.new(number: number.to_i, suit: suit)
-      end
+    input
+      .scan(/(?<numbers>[1-9]+)(?<suit>[mpsz])/)
+      .flat_map do |numbers, suit|
+      numbers.each_char.map { |number| Tile.new(number: number.to_i, suit: suit) }
     end
   end
 end
--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -4,32 +4,90 @@ class ProblemTest < ActiveSupport::TestCase
   test "should return hand tiles with suits added" do
     problem = Problem.new(hand: "44m667p123678s666z", solution: "6p")
 
-    hand_tiles = problem.hand_tiles_to_s
+    hand_tiles = problem.hand_tiles
 
-    assert_equal "4m4m6p6p7p1s2s3s6s7s8s6z6z6z", hand_tiles
+    p hand_tiles
+
+    expected_tiles = [
+      Tile.new(number: 4, suit: "m"),
+      Tile.new(number: 4, suit: "m"),
+      Tile.new(number: 6, suit: "p"),
+      Tile.new(number: 6, suit: "p"),
+      Tile.new(number: 7, suit: "p"),
+      Tile.new(number: 1, suit: "s"),
+      Tile.new(number: 2, suit: "s"),
+      Tile.new(number: 3, suit: "s"),
+      Tile.new(number: 6, suit: "s"),
+      Tile.new(number: 7, suit: "s"),
+      Tile.new(number: 8, suit: "s"),
+      Tile.new(number: 6, suit: "z"),
+      Tile.new(number: 6, suit: "z"),
+      Tile.new(number: 6, suit: "z")
+    ]
+
+    assert_equal expected_tiles, hand_tiles
   end
 
   test "should return solution tiles with 'OR' added" do
     problem = Problem.new(hand: "5677m34p45579s666z", solution: "7m4s")
 
-    solution_tiles = problem.solution_tiles_to_s
+    solution_tiles = problem.solution_tiles
 
-    assert_equal "7m or 4s", solution_tiles
+    expected_tiles = [
+      Tile.new(number: 7, suit: "m"),
+      Tile.new(number: 4, suit: "s")
+    ]
+
+    assert_equal expected_tiles, solution_tiles
   end
 
   test "should return hand tiles in original order" do
     problem = Problem.new(hand: "2p343s44455z111m22p", solution: "3s")
 
-    hand_tiles = problem.hand_tiles_to_s
+    hand_tiles = problem.hand_tiles
 
-    assert_equal "2p3s4s3s4z4z4z5z5z1m1m1m2p2p", hand_tiles
+    expected_tiles = [
+      Tile.new(number: 2, suit: "p"),
+      Tile.new(number: 3, suit: "s"),
+      Tile.new(number: 4, suit: "s"),
+      Tile.new(number: 3, suit: "s"),
+      Tile.new(number: 4, suit: "z"),
+      Tile.new(number: 4, suit: "z"),
+      Tile.new(number: 4, suit: "z"),
+      Tile.new(number: 5, suit: "z"),
+      Tile.new(number: 5, suit: "z"),
+      Tile.new(number: 1, suit: "m"),
+      Tile.new(number: 1, suit: "m"),
+      Tile.new(number: 1, suit: "m"),
+      Tile.new(number: 2, suit: "p"),
+      Tile.new(number: 2, suit: "p")
+    ]
+
+    assert_equal expected_tiles, hand_tiles
   end
 
   test "should return hand tiles when given longhand format" do
     problem = Problem.new(hand: "1m2m3m4m5p6p7p8p9s9s1z1z2z2z", solution: "9s")
 
-    hand_tiles = problem.hand_tiles_to_s
+    hand_tiles = problem.hand_tiles
 
-    assert_equal "1m2m3m4m5p6p7p8p9s9s1z1z2z2z", hand_tiles
+    expected_tiles = [
+      Tile.new(number: 1, suit: "m"),
+      Tile.new(number: 2, suit: "m"),
+      Tile.new(number: 3, suit: "m"),
+      Tile.new(number: 4, suit: "m"),
+      Tile.new(number: 5, suit: "p"),
+      Tile.new(number: 6, suit: "p"),
+      Tile.new(number: 7, suit: "p"),
+      Tile.new(number: 8, suit: "p"),
+      Tile.new(number: 9, suit: "s"),
+      Tile.new(number: 9, suit: "s"),
+      Tile.new(number: 1, suit: "z"),
+      Tile.new(number: 1, suit: "z"),
+      Tile.new(number: 2, suit: "z"),
+      Tile.new(number: 2, suit: "z")
+    ]
+
+    assert_equal expected_tiles, hand_tiles
   end
 end
