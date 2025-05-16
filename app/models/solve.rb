8b0224174ec9d9e40256e4549c67d7cc9d6ca8f1
packagespace <packagespace@pm.me>
2025-05-16 14:16:21 +0200

    Add solve tests

--- app/models/solve.rb
+++ app/models/solve.rb
@@ -2,10 +2,32 @@ class Solve < ApplicationRecord
   belongs_to :problem
   belongs_to :user, optional: true
 
-  validates :tile_notation, presence: true
+  validates :tile_notation, presence: true, length: { maximum: 255 }
+  validates :problem, presence: true
+  validate :valid_tile
 
   def to_tile
-    number, suit = tile_notation.scan(/([0-9])([mpsz])/)[0]
+    number, suit = tile_notation.scan(/\A(?<number>\d+)(?<suit>[a-z])\z/)[0]
     Tile.new(number: number.to_i, suit: suit)
+  end
+
+  private
+
+  def valid_tile
+    return if tile_notation.blank?
+
+    validate_notation_format
+    return if errors.include?(:tile_notation)
+
+    tile = to_tile
+    unless tile.valid?
+      errors.add(:tile_notation, message: tile.errors.full_messages.join(", "))
+    end
+  end
+
+  def validate_notation_format
+    unless tile_notation.match?(/\A\d+[a-z]\z/)
+      errors.add(:tile_notation, message: "must be in format like '1m'")
+    end
   end
 end
--- test/models/solve_test.rb
+++ test/models/solve_test.rb
@@ -1,7 +1,79 @@
 require "test_helper"
 
 class SolveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @problem = problems(:one) # Assume you have this fixture
+    @solve = Solve.new(
+      problem: @problem,
+      tile_notation: "1m"
+    )
+  end
+
+  test "should be valid with valid attributes" do
+    assert @solve.valid?
+  end
+
+  ### Association Tests ###
+
+  test "should belong to problem" do
+    @solve.problem = nil
+    refute @solve.valid?
+    assert_includes @solve.errors[:problem], "must exist"
+  end
+
+  test "should optionally belong to user" do
+    @solve.user = nil
+    assert @solve.valid?
+
+    @solve.user = users(:one) # Assume you have this fixture
+    assert @solve.valid?
+  end
+
+  ### Validation Tests ###
+
+  test "should require tile_notation" do
+    @solve.tile_notation = nil
+    refute @solve.valid?
+    assert_includes @solve.errors[:tile_notation], "can't be blank"
+  end
+
+  test "should validate tile_notation format" do
+    invalid_formats = {
+      "ab" => "no numbers",
+      "12" => "no suit",
+      "m1" => "wrong order"
+    }
+
+    invalid_formats.each do |format, description|
+      @solve.errors.clear
+      @solve.tile_notation = format
+      refute @solve.valid?
+      assert_includes @solve.errors[:tile_notation], "must be in format like '1m'"
+    end
+  end
+
+  test "should accept valid tile notations" do
+    valid_formats = %w[1m 9p 5s 7z]
+
+    valid_formats.each do |format|
+      @solve.tile_notation = format
+      assert @solve.valid?, "#{format} should be valid"
+    end
+  end
+
+  ### to_tile Method Tests ###
+
+  test "to_tile should return valid Tile object" do
+    @solve.tile_notation = "1m"
+    tile = @solve.to_tile
+
+    assert_instance_of Tile, tile
+    assert_equal 1, tile.number
+    assert_equal "m", tile.suit
+  end
+
+  test "should validate tile is valid" do
+    @solve.tile_notation = "0w"
+    refute @solve.valid?
+  end
 end
