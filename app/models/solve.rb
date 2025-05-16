773cc67873ae0655a87df4f6526a7151f2103024
packagespace <packagespace@pm.me>
2025-05-16 14:24:45 +0200

    Add more validation

--- app/models/solve.rb
+++ app/models/solve.rb
@@ -4,7 +4,7 @@ class Solve < ApplicationRecord
 
   validates :tile_notation, presence: true, length: { maximum: 255 }
   validates :problem, presence: true
-  validate :valid_tile
+  validate :valid_solve_tile
 
   def to_tile
     number, suit = tile_notation.scan(/\A(?<number>\d+)(?<suit>[a-z])\z/)[0]
@@ -13,7 +13,7 @@ class Solve < ApplicationRecord
 
   private
 
-  def valid_tile
+  def valid_solve_tile
     return if tile_notation.blank?
 
     validate_notation_format
@@ -22,6 +22,16 @@ class Solve < ApplicationRecord
     tile = to_tile
     unless tile.valid?
       errors.add(:tile_notation, message: tile.errors.full_messages.join(", "))
+      return
+    end
+    validate_tile_in_problem_hand
+  end
+
+  def validate_tile_in_problem_hand
+    return if errors.include?(:problem)
+
+    unless problem.hand_tiles.include?(to_tile)
+      errors.add(:tile_notation, message: "tile #{tile_notation} is not in the problem's hand")
     end
   end
 
--- test/models/solve_test.rb
+++ test/models/solve_test.rb
@@ -5,7 +5,7 @@ class SolveTest < ActiveSupport::TestCase
     @problem = problems(:one) # Assume you have this fixture
     @solve = Solve.new(
       problem: @problem,
-      tile_notation: "1m"
+      tile_notation: "7m"
     )
   end
 
@@ -52,15 +52,6 @@ class SolveTest < ActiveSupport::TestCase
     end
   end
 
-  test "should accept valid tile notations" do
-    valid_formats = %w[1m 9p 5s 7z]
-
-    valid_formats.each do |format|
-      @solve.tile_notation = format
-      assert @solve.valid?, "#{format} should be valid"
-    end
-  end
-
   ### to_tile Method Tests ###
 
   test "to_tile should return valid Tile object" do
@@ -75,5 +66,44 @@ class SolveTest < ActiveSupport::TestCase
   test "should validate tile is valid" do
     @solve.tile_notation = "0w"
     refute @solve.valid?
+  end
+
+  test "should validate tile exists in problem hand" do
+    problem = Problem.new(
+      title: "Test Problem",
+      hand_notation: "123m456p789s12345z",
+      solution_notation: "123m"
+    )
+    problem.save!
+
+    solve = Solve.new(
+      problem: problem,
+      tile_notation: "4m"  # tile not in hand
+    )
+
+    refute solve.valid?
+    assert_includes solve.errors[:tile_notation], "tile 4m is not in the problem's hand"
+  end
+
+  test "should accept tile that exists in problem hand" do
+    problem = Problem.new(
+      title: "Test Problem",
+      hand_notation: "123m456p789s12345z",
+      solution_notation: "123m"
+    )
+    problem.save!
+
+    solve = Solve.new(
+      problem: problem,
+      tile_notation: "1m"  # tile in hand
+    )
+
+    assert solve.valid?
+  end
+
+  test "should handle missing problem gracefully" do
+    solve = Solve.new(tile_notation: "1m")
+    refute solve.valid?
+    assert_includes solve.errors[:problem], "must exist"
   end
 end
